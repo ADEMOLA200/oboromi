0de27edc3510052195c795aa0ac3b3b3af05105c
xinitrcn1 <239304139+xinitrcn1@users.noreply.github.com>
2025-11-26 23:18:16 +0000

    [sm86] AL2P emit, extra enum defs

--- core/src/gpu/sm86.rs
+++ core/src/gpu/sm86.rs
@@ -2,23 +2,234 @@
 
 use crate::gpu::spirv;
 
+static MAX_REG_COUNT: usize = 254;
+static MAX_UNIFORM_REG_COUNT: usize = 63;
+static MAX_CONST_BANK: usize = 17;
+static ALLOW_F16_PARTIAL_WRITES: usize = 1;
+
+enum TextureType {
+    ONE_D = 0,
+    TWO_D = 1,
+    THREE_D = 2,
+    CUBEMAP = 3,
+    ONE_D_ARRAY = 4,
+    TWO_D_ARRAY = 5,
+    ONE_D_BUFFER = 6,
+    TWO_D_NO_MIPMAP = 7,
+    CUBE_ARRAY = 8,
+}
+
+enum TicSource {
+    ZERO = 0,
+    R = 2,
+    G = 3,
+    B = 4,
+    A = 5,
+    ONE_INT = 6,
+    ONE_FLOAT = 7,
+}
+
+enum TicType {
+    SNORM = 1,
+    UNORM = 2,
+    SINT = 3,
+    UINT = 4,
+    SNORM_FORCE_FP16 = 5,
+    UNORM_FORCE_FP16 = 6,
+    FLOAT = 7,
+}
+
+enum MaxAnisotropy {
+    _1_TO_1 = 0x0,
+    _2_TO_1 = 0x1,
+    _4_TO_1 = 0x2,
+    _6_TO_1 = 0x3,
+    _8_TO_1 = 0x4,
+    _10_TO_1 = 0x5,
+    _12_TO_1 = 0x6,
+    _16_TO_1 = 0x7,
+}
+
+#[repr(u32)]
+enum SurfaceFormat {
+    BITMAP = 0x001c,
+    UNK1D = 0x001d,
+    RGBA32_FLOAT = 0x00c0,
+    RGBA32_SINT = 0x00c1,
+    RGBA32_UINT = 0x00c2,
+    RGBX32_FLOAT = 0x00c3,
+    RGBX32_SINT = 0x00c4,
+    RGBX32_UINT = 0x00c5,
+    RGBA16_UNORM = 0x00c6,
+    RGBA16_SNORM = 0x00c7,
+    RGBA16_SINT = 0x00c8,
+    RGBA16_UINT = 0x00c9,
+    RGBA16_FLOAT = 0x00ca,
+    RG32_FLOAT = 0x00cb,
+    RG32_SINT = 0x00cc,
+    RG32_UINT = 0x00cd,
+    RGBX16_FLOAT = 0x00ce,
+    BGRA8_UNORM = 0x00cf,
+    BGRA8_SRGB = 0x00d0,
+    RGB10_A2_UNORM = 0x00d1,
+    RGB10_A2_UINT = 0x00d2,
+    RGBA8_UNORM = 0x00d5,
+    RGBA8_SRGB = 0x00d6,
+    RGBA8_SNORM = 0x00d7,
+    RGBA8_SINT = 0x00d8,
+    RGBA8_UINT = 0x00d9,
+    RG16_UNORM = 0x00da,
+    RG16_SNORM = 0x00db,
+    RG16_SINT = 0x00dc,
+    RG16_UINT = 0x00dd,
+    RG16_FLOAT = 0x00de,
+    BGR10_A2_UNORM = 0x00df,
+    R11G11B10_FLOAT = 0x00e0,
+    R32_SINT = 0x00e3,
+    R32_UINT = 0x00e4,
+    R32_FLOAT = 0x00e5,
+    BGRX8_UNORM = 0x00e6,
+    BGRX8_SRGB = 0x00e7,
+    B5G6R5_UNORM = 0x00e8,
+    BGR5_A1_UNORM = 0x00e9,
+    RG8_UNORM = 0x00ea,
+    RG8_SNORM = 0x00eb,
+    RG8_SINT = 0x00ec,
+    RG8_UINT = 0x00ed,
+    R16_UNORM = 0x00ee,
+    R16_SNORM = 0x00ef,
+    R16_SINT = 0x00f0,
+    R16_UINT = 0x00f1,
+    R16_FLOAT = 0x00f2,
+    R8_UNORM = 0x00f3,
+    R8_SNORM = 0x00f4,
+    R8_SINT = 0x00f5,
+    R8_UINT = 0x00f6,
+    A8_UNORM = 0x00f7,
+    BGR5_X1_UNORM = 0x00f8,
+    RGBX8_UNORM = 0x00f9,
+    RGBX8_SRGB = 0x00fa,
+    BGR5_X1_UNORM_UNKFB = 0x00fb,
+    BGR5_X1_UNORM_UNKFC = 0x00fc,
+    BGRX8_UNORM_UNKFD = 0x00fd,
+    BGRX8_UNORM_UNKFE = 0x00fe,
+    Y32_UINT_UNKFF = 0x00ff,
+}
+
+enum ZetaFormat {
+    Z32_FLOAT = 0x000a,
+    Z16_UNORM = 0x0013,
+    S8_Z24_UNORM = 0x0014,
+    Z24_X8_UNORM = 0x0015,
+    Z24_S8_UNORM = 0x0016,
+    Z24_C8_UNORM = 0x0018,
+    Z32_S8_X24_FLOAT = 0x0019,
+    Z24_X8_S8_C8_X16_UNORM = 0x001d,
+    Z32_X8_C8_X16_FLOAT = 0x001e,
+    Z32_S8_C8_X16_FLOAT = 0x001f,
+}
+
+// SUTP
+enum ImageFormat {
+    RGBA32_FLOAT = 0x02,
+    RGBA32_SINT = 0x03,
+    RGBA32_UINT = 0x04,
+    RGBA16_UNORM = 0x08,
+    RGBA16_SNORM = 0x09,
+    RGBA16_SINT = 0x0a,
+    RGBA16_UINT = 0x0b,
+    RGBA16_FLOAT = 0x0c,
+    RG32_FLOAT = 0x0d,
+    RG32_SINT = 0x0e,
+    RG32_UINT = 0x0f,
+    BGRA8_UNORM = 0x11,
+    RGB10_A2_UNORM = 0x13,
+    RGB10_A2_UINT = 0x15,
+    RGBA8_UNORM = 0x18,
+    RGBA8_SNORM = 0x1a,
+    RGBA8_SINT = 0x1b,
+    RGBA8_UINT = 0x1c,
+    RG16_UNORM = 0x1d,
+    RG16_SNORM = 0x1e,
+    RG16_SINT = 0x1f,
+    RG16_UINT = 0x20,
+    RG16_FLOAT = 0x21,
+    R11G11B10_FLOAT = 0x24,
+    R32_SINT = 0x27,
+    R32_UINT = 0x28,
+    R32_FLOAT = 0x29,
+    RG8_UNORM = 0x2e,
+    RG8_SNORM = 0x2f,
+    RG8_SINT = 0x30,
+    RG8_UINT = 0x31,
+    R16_UNORM = 0x32,
+    R16_SNORM = 0x33,
+    R16_SINT = 0x34,
+    R16_UINT = 0x35,
+    R16_FLOAT = 0x36,
+    R8_UNORM = 0x37,
+    R8_SNORM = 0x38,
+    R8_SINT = 0x39,
+    R8_UINT = 0x3a,
+}
+
+enum BitSize {
+    B32 = 0b00,
+    B64 = 0b01,
+    B96 = 0b10,
+    B128 = 0b11,
+}
+
 pub struct Decoder<'a> {
     pub ir: &'a mut spirv::Emitter,
+    // Declared types for headers
+    type_u8: u32,
+    type_u16: u32,
+    type_u32: u32,
+    type_u64: u32,
+    type_s8: u32,
+    type_s16: u32,
+    type_s32: u32,
+    type_s64: u32,
+    type_f16: u32,
+    type_f32: u32,
+    type_f64: u32,
 }
 impl<'a> Decoder<'a> {
+    pub fn init(&mut self) {
+        self.type_u8 = self.ir.emit_type_int(8, 0);
+        self.type_u16 = self.ir.emit_type_int(16, 0);
+        self.type_u32 = self.ir.emit_type_int(32, 0);
+        self.type_u64 = self.ir.emit_type_int(64, 0);
+        self.type_s8 = self.ir.emit_type_int(8, 1);
+        self.type_s16 = self.ir.emit_type_int(16, 1);
+        self.type_s32 = self.ir.emit_type_int(32, 1);
+        self.type_s64 = self.ir.emit_type_int(64, 1);
+        self.type_f16 = self.ir.emit_type_float(16);
+        self.type_f32 = self.ir.emit_type_float(32);
+        self.type_f64 = self.ir.emit_type_float(64);
+    }
+    pub fn finish(&mut self) {
+
+    }
+
     pub fn al2p(&mut self, inst: u128) {
         let _pg = (((inst >> 12) & 0x7) << 0);
         let _pg_not = (((inst >> 15) & 0x1) << 0);
         let _rd = (((inst >> 16) & 0xff) << 0);
-        let _ra = (((inst >> 24) & 0xff) << 0);
-        let _ra_offset = (((inst >> 40) & 0x7ff) << 0);
-        let _bop = (((inst >> 74) & 0x3) << 0);
+        let ra = (((inst >> 24) & 0xff) << 0) as usize;
+        let ra_offset = (((inst >> 40) & 0x7ff) << 0) as usize;
+        let bop = (((inst >> 74) & 0x3) << 0) as usize;
         let _op = (((inst >> 79) & 0x1) << 0);
         let _pm_pred = (((inst >> 102) & 0x3) << 0);
         let _dst_wr_sb = (((inst >> 110) & 0x7) << 0);
         let _src_rel_sb = (((inst >> 113) & 0x7) << 0);
         let _req_bit_set = (((inst >> 116) & 0x3f) << 0);
         let _opex = (((inst >> 122) & 0x7) << 5) | (((inst >> 105) & 0x1f) << 0);
+        // %rd := %ra + $ra_offset
+        assert!(ra <= MAX_REG_COUNT);
+        assert!(bop == BitSize::B32 as usize);
+        let ir_imm32 = self.ir.emit_constant_typed::<u32>(self.type_u32, ra_offset as u32);
         todo!();
     }
     pub fn ald(&mut self, inst: u128) {
--- core/src/gpu/spirv.rs
+++ core/src/gpu/spirv.rs
@@ -1,13 +1,43 @@
 //! SPIR-V IR emitter for GPU instructions
+// https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html
+
+// Hopefully heap ellision kicks in, knowing rust codegen that may not happen at all :)
+trait SpirvNumericLiteral {
+    fn to_literal(self) -> Vec<u32>;
+}
+impl SpirvNumericLiteral for u8 {
+    fn to_literal(self) -> Vec<u32> {
+        vec![self as u32]
+    }
+}
+impl SpirvNumericLiteral for u16 {
+    fn to_literal(self) -> Vec<u32> {
+        vec![self as u32]
+    }
+}
+impl SpirvNumericLiteral for u32 {
+    fn to_literal(self) -> Vec<u32> {
+        vec![self]
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct Emitter {
     code: Vec<u32>,
+    id_count: u32,
 }
 impl Emitter {
     pub fn new() -> Self {
-        Self { code: Vec::new() }
+        Self{
+            code: Vec::default(),
+            id_count: 0,
+        }
     }
+    pub fn new_id(&mut self) -> u32 {
+        self.id_count += 1;
+        self.id_count - 1
+    }
+
     pub fn emit_header(&mut self) {
         self.code.push(0x07230203);
     }
@@ -24,8 +54,10 @@ impl Emitter {
     pub fn emit_nop(&mut self) {
         self.emit_generic(0, &[]);
     }
-    pub fn emit_undef(&mut self, result_type: u32, result: u32) {
+    pub fn emit_undef(&mut self, result_type: u32) -> u32 {
+        let result = self.new_id();
         self.emit_generic(1, &[result_type, result]);
+        result
     }
     pub fn emit_decorate(&mut self, target: u32, deco: u32, literals: &[u32]) {
         let len = 3 + u32::try_from(literals.len()).unwrap();
@@ -46,8 +78,10 @@ impl Emitter {
             self.code.push(l);
         }
     }
-    pub fn emit_decoration_group(&mut self, result: u32) {
+    pub fn emit_decoration_group(&mut self) -> u32 {
+        let result = self.new_id();
         self.emit_generic(73, &[result]);
+        result
     }
     pub fn emit_group_decorate(&mut self, group: u32, targets: &[u32]) {
         let len = 2 + u32::try_from(targets.len()).unwrap();
@@ -57,39 +91,41 @@ impl Emitter {
             self.code.push(l);
         }
     }
-    pub fn emit_type_void(&mut self, result: u32) {
+    pub fn emit_type_void(&mut self) -> u32 {
+        let result = self.new_id();
         self.emit_generic(19, &[result]);
+        result
     }
-    pub fn emit_type_bool(&mut self, result: u32) {
+    pub fn emit_type_bool(&mut self) -> u32 {
+        let result = self.new_id();
         self.emit_generic(20, &[result]);
+        result
     }
-    pub fn emit_type_inst(&mut self, result: u32, width: u32, sign: u32) {
+    pub fn emit_type_int(&mut self, width: u32, sign: u32) -> u32 {
+        let result = self.new_id();
         assert!(sign == 0 || sign == 1);
         self.emit_generic(21, &[result, width, sign]);
+        result
     }
-    pub fn emit_type_float(&mut self, result: u32, width: u32) {
+    pub fn emit_type_float(&mut self, width: u32) -> u32 {
+        let result = self.new_id();
         self.emit_generic(22, &[result, width]);
+        result
     }
-    pub fn emit_type_vector(&mut self, result: u32, type_: u32, count: u32) {
+    pub fn emit_type_vector(&mut self, type_: u32, count: u32) -> u32 {
+        let result = self.new_id();
         assert!(count >= 2);
         self.emit_generic(23, &[result, type_, count]);
+        result
     }
-    pub fn emit_type_matrix(&mut self, result: u32, type_: u32, count: u32) {
+    pub fn emit_type_matrix(&mut self, type_: u32, count: u32) -> u32 {
+        let result = self.new_id();
         assert!(count >= 2);
         self.emit_generic(24, &[result, type_, count]);
+        result
     }
-    pub fn emit_type_image(
-        &mut self,
-        result: u32,
-        type_: u32,
-        dim: u32,
-        depth: u32,
-        arrayed: u32,
-        ms: u32,
-        sampled: u32,
-        format: u32,
-        acc_qual: &[u32],
-    ) {
+    pub fn emit_type_image(&mut self, type_: u32, dim: u32, depth: u32, arrayed: u32, ms: u32, sampled: u32, format: u32, acc_qual: &[u32]) -> u32 {
+        let result = self.new_id();
         assert!(depth <= 2);
         assert!(arrayed == 0 || arrayed == 1);
         assert!(ms == 0 || ms == 1);
@@ -99,15 +135,56 @@ impl Emitter {
             data.push(e);
         }
         self.emit_generic(25, &data);
+        result
     }
-    pub fn emit_type_sampler(&mut self, result: u32) {
+    pub fn emit_type_sampler(&mut self) -> u32 {
+        let result = self.new_id();
         self.emit_generic(26, &[result]);
+        result
     }
-    pub fn emit_type_sampled_image(&mut self, result: u32, type_: u32) {
+    pub fn emit_type_sampled_image(&mut self, type_: u32) -> u32 {
+        let result = self.new_id();
         self.emit_generic(27, &[result, type_]);
+        result
     }
-    pub fn emit_type_array(&mut self, result: u32, type_: u32, length: u32) {
+    pub fn emit_type_array(&mut self, type_: u32, length: u32) -> u32 {
+        let result = self.new_id();
         self.emit_generic(28, &[result, type_, length]);
+        result
+    }
+    pub fn emit_type_runtime_array(&mut self, type_: u32) -> u32 {
+        let result = self.new_id();
+        self.emit_generic(29, &[result, type_]);
+        result
+    }
+
+    // TODO: 29 to 42
+
+    pub fn emit_constant_true(&mut self, type_: u32) -> u32 {
+        let result = self.new_id();
+        self.emit_generic(41, &[type_, result]);
+        result
+    }
+    pub fn emit_constant_false(&mut self, type_: u32) -> u32 {
+        let result = self.new_id();
+        self.emit_generic(42, &[type_, result]);
+        result
+    }
+    pub fn emit_constant(&mut self, type_: u32, value: &[u32]) -> u32 {
+        let result = self.new_id();
+        let mut data = vec![type_, result];
+        for &e in value {
+            data.push(e);
+        }
+        self.emit_generic(43, &data);
+        result
+    }
+
+    // Helper functions -- no 1:1 map
+    pub fn emit_constant_typed<T: SpirvNumericLiteral>(&mut self, type_: u32, value: u32) -> u32 {
+        let result = self.new_id();
+        self.emit_constant(type_, &value.to_literal());
+        result
     }
 }
 
